ef6786c1344f2fc7649877d2a6cf006e6671422b
Sama8-glitch <Sama_8@icloud.com>
2026-07-14 08:46:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,21 @@ const heart = document.getElementById("heart");
 
 let output = "";
 
-for (let y = 1.5; y > -1.5; y -= 0.05) {
+for (let y = 1.5; y > -1.5; y -= 0.08) {
     let line = "";
 
-    for (let x = -1.5; x < 1.5; x += 0.025) {
+    for (let x = -1.5; x < 1.5; x += 0.04) {
 
         let a = x * x + y * y - 1;
 
         if (a * a * a - x * x * y * y * y <= 0) {
-
             line += "I love you ";
-
         } else {
-
-            line += "           ";
-
+            line += "           "; // نفس عدد حروف I love you
         }
-
     }
 
     output += line + "\n";
-
 }
 
 heart.textContent = output;
